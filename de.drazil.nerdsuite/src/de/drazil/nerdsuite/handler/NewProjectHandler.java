20ed7957bc5d5b98e7b9982f642e0ab5ca29a020
guidobonerz <guido.bonerz@drazil.de>
2018-12-20 03:35:13 +0100

    update

--- de.drazil.nerdsuite/src/de/drazil/nerdsuite/handler/NewProjectHandler.java
+++ de.drazil.nerdsuite/src/de/drazil/nerdsuite/handler/NewProjectHandler.java
@@ -1,7 +1,5 @@
 package de.drazil.nerdsuite.handler;
 
-import javax.inject.Inject;
-
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.workbench.IWorkbench;
@@ -13,18 +11,13 @@ import de.drazil.nerdsuite.explorer.Explorer;
 import de.drazil.nerdsuite.model.Project;
 import de.drazil.nerdsuite.wizard.ProjectWizard;
 
-public class NewProjectHandler
-{
-	@Inject
-	EPartService partService;
+public class NewProjectHandler {
 
 	@Execute
-	public void execute(IWorkbench workbench, Shell shell)
-	{
+	public void execute(IWorkbench workbench, Shell shell, EPartService partService) {
 		ProjectWizard projectWizard = new ProjectWizard();
 		WizardDialog wizardDialog = new WizardDialog(shell, projectWizard);
-		if (wizardDialog.open() == WizardDialog.OK)
-		{
+		if (wizardDialog.open() == WizardDialog.OK) {
 			Project project = projectWizard.getProject();
 			MPart part = partService.findPart("de.drazil.nerdsuite.part.projectbrowser");
 			Explorer explorer = (Explorer) part.getObject();
