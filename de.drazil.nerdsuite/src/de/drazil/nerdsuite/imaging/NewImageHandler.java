7195dfe38a4c2047f92cad9cb40f817a21793fa9
guidobonerz <guido.bonerz@drazil.de>
2018-12-21 09:43:11 +0100

    update app layout

---
+++ de.drazil.nerdsuite/src/de/drazil/nerdsuite/imaging/NewImageHandler.java
@@ -0,0 +1,15 @@
+
+package de.drazil.nerdsuite.imaging;
+
+import javax.inject.Named;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public class NewImageHandler {
+	@Execute
+	public void execute(@Named("de.drazil.nerdsuite.commandparameter.width") String width,
+			@Named("de.drazil.nerdsuite.commandparameter.width") String height) {
+		System.out.println(width + ":" + height);
+	}
+
+}
