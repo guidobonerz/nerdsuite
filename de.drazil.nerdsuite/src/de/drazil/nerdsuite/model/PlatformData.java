561e6832f55c748e9b9a6f1e3f81fdcda105c6c9
guidobonerz <guido.bonerz@drazil.de>
2019-08-14 21:47:50 +0200

    update config structure

--- de.drazil.nerdsuite/src/de/drazil/nerdsuite/model/PlatformData.java
+++ de.drazil.nerdsuite/src/de/drazil/nerdsuite/model/PlatformData.java
@@ -8,9 +8,7 @@ import lombok.Data;
 public class PlatformData {
 	private String cpuInstructionSource;
 	private String basicInstructionSource;
-	private int basicVersion;
 	private String imagingServiceClass;
-	private List<ImageType> imageTypes;
 	private List<Address> platformAddressList;
 	private List<Pointer> platformPointerList;
 	private List<PlatformColor> colorPalette;
