5bb0407eef554c5fa38eb42b1b983605243cb1f5
guidobonerz <guido.bonerz@drazil.de>
2019-09-01 13:12:25 +0200

    atr read subfolders

--- de.drazil.nerdsuite/src/de/drazil/nerdsuite/storagemedia/ATR_MediaManager.java
+++ de.drazil.nerdsuite/src/de/drazil/nerdsuite/storagemedia/ATR_MediaManager.java
@@ -19,9 +19,7 @@ public class ATR_MediaManager extends AbstractBaseMediaManager {
 
 	private int directorySector;
 	private int largeDiskCorrectionCount;
-	private int directoryBaseOffset;
-	private int currentDirectoryEntryOffset;
-	private int currentDirectorySectorOffset;
+	// private int directoryBaseOffset;
 
 	public ATR_MediaManager(File file) {
 		super(file);
@@ -37,31 +35,37 @@ public class ATR_MediaManager extends AbstractBaseMediaManager {
 		diskFlag = getByte(8);
 		firstTypicalSector = getWord(9);
 		vtocStartOffset = 0x167;
-
 		typeCode = content[vtocStartOffset];
 		sectorCount = getWord(vtocStartOffset + 0x01);
 		unusedSectorCount = getWord(vtocStartOffset + 0x03);
-
 		directorySector = 0x168;
 		largeDiskCorrectionCount = 3;
-		directoryBaseOffset = (sectorSize == 0x80 ? (directorySector * sectorSize)
-				: (largeDiskCorrectionCount * 0x80 + (directorySector - largeDiskCorrectionCount) * sectorSize)) + 0x10;
-		currentDirectorySectorOffset = directoryBaseOffset;
+		getRoot().setSector(directorySector);
+	}
 
+	private int getSectorOffset(int sector) {
+		return (sectorSize == 0x80 ? (sector * sectorSize)
+				: (largeDiskCorrectionCount * 0x80 + (sector - largeDiskCorrectionCount) * sectorSize)) + 0x10;
 	}
 
 	@Override
 	protected void readEntries(MediaEntry parent) {
 		int id = 1;
 		boolean hasMoreEntries = true;
-		currentDirectoryEntryOffset = currentDirectorySectorOffset;
-		while (hasMoreEntries) {
+		int currentDirectorySectorOffset = getSectorOffset(parent.getSector());
+		if (parent.isDirectory()) {
+			currentDirectorySectorOffset -= 0x100;
+		}
+		int currentDirectoryEntryOffset = currentDirectorySectorOffset;
 
+		while (hasMoreEntries) {
 			int entryFlag = content[currentDirectoryEntryOffset];
 			int entrySectorCount = getWord(currentDirectoryEntryOffset + 0x01);
 			int entrySector = getWord(currentDirectoryEntryOffset + 0x03);
-			int subfolderOffset = (sectorSize == 0x80 ? (entrySector * sectorSize)
-					: (3 * 0x80 + entrySector * sectorSize)) + 0x10 - 0x270;
+			/*
+			 * int subfolderOffset = (sectorSize == 0x80 ? (entrySector * sectorSize) : (3 *
+			 * 0x80 + entrySector * sectorSize)) + 0x10 - 0x270;
+			 */
 			String fileName = getString(currentDirectoryEntryOffset + 0x05, currentDirectoryEntryOffset + 0x0c, false);
 			String fileExtension = getString(currentDirectoryEntryOffset + 0x0d, currentDirectoryEntryOffset + 0x0f,
 					false);
@@ -71,20 +75,18 @@ public class ATR_MediaManager extends AbstractBaseMediaManager {
 				MediaEntry entry = new MediaEntry(id, fileName, fileName, fileExtension, 0, 0, 0, 0, null);
 				entry.setDirectory((entryFlag & 0x10) == 0x10);
 				entry.setUserObject(getContainer());
+
+				entry.setSector(entrySector);
 				MediaMountFactory.addChildEntry(parent, entry);
 			}
 			currentDirectoryEntryOffset += 0x10;
-
 			if (id % 8 == 0) {
 				currentDirectorySectorOffset += sectorSize;
 				currentDirectoryEntryOffset = currentDirectorySectorOffset;
 			}
-
 			hasMoreEntries = !isEmptyEntry(currentDirectoryEntryOffset, 0x10);
-
 			id++;
 		}
-
 	}
 
 	@Override
