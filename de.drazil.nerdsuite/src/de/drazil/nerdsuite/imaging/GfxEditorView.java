7d2f8778dbfb9fad3698df415c4813be3b12d564
guidobonerz <guido.bonerz@drazil.de>
2019-09-12 06:03:59 +0200

    updates

--- de.drazil.nerdsuite/src/de/drazil/nerdsuite/imaging/GfxEditorView.java
+++ de.drazil.nerdsuite/src/de/drazil/nerdsuite/imaging/GfxEditorView.java
@@ -219,7 +219,7 @@ public class GfxEditorView // implements IConfigurationListener {
 				.setGraphicFormat((GraphicFormat) ((Map<String, Object>) part.getObject()).get("gfxFormat"), 0);
 		
 		tileRepositoryService = ServiceFactory.getService(getOwner(), TileRepositoryService.class);
-		menuService.registerContextMenu(getPainterWidget(), "de.drazil.nerdsuite.popupmenu.GfxToolbox");
+		menuService.registerContextMenu(getPainterWidget(), "de.drazil.nerdsuite.menu.0");
 		parent.setLayout(new MigLayout());
 		getPainterWidget().setLayoutData("span 6 6");
 
