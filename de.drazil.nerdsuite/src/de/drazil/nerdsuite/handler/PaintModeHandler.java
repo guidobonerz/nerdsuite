74634190c954d084d219ebc40f63b6e8446d45ef
guidobonerz <guido.bonerz@drazil.de>
2019-09-13 09:23:11 +0200

    PaintMode

--- de.drazil.nerdsuite/src/de/drazil/nerdsuite/handler/PaintModeHandler.java
+++ de.drazil.nerdsuite/src/de/drazil/nerdsuite/handler/PaintModeHandler.java
@@ -7,13 +7,16 @@ import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.model.application.ui.menu.MToolItem;
 
-import de.drazil.nerdsuite.constants.PaintMode;
-import de.drazil.nerdsuite.imaging.GfxEditorView;
-
 public class PaintModeHandler {
 	@Execute
-	public void execute(final MToolItem item, MPart part,
+	public void execute(MToolItem item, MPart part,
 			@Named("de.drazil.nerdsuite.commandparameter.PaintMode") String paintMode) {
-		((GfxEditorView) part.getObject()).controlPaintMode(PaintMode.values()[Integer.valueOf(paintMode)]);
+		// ShiftService service = ServiceFactory.getService((String)
+		// part.getTransientData().get(Constants.OWNER),ShiftService.class);
+		// service.setImagingWidgetConfiguration((ImagingWidgetConfiguration)
+		// part.getTransientData().get("CONFIG"));
+		// service.execute(Integer.valueOf(direction));
+		System.out.println("paint mode:" + paintMode);
 	}
+
 }
