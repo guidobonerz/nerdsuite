8bc5727fae25fc1b8c3cea43bf73d77ad42e8c5d
guidobonerz <guido.bonerz@drazil.de>
2021-01-01 02:36:19 +0100

    update

--- de.drazil.nerdsuite/src/de/drazil/nerdsuite/imaging/service/FlipService.java
+++ de.drazil.nerdsuite/src/de/drazil/nerdsuite/imaging/service/FlipService.java
@@ -13,27 +13,36 @@ public class FlipService extends AbstractImagingService {
 	@Override
 	public void each(int action, int tileIndex, Tile tile, TileRepositoryService repositoryService, TileAction tileAction) {
 		int[] content = repositoryService.getActiveLayerFromSelectedTile().getContent();
-		Rectangle r = service.getSelection();
+		int[] brush = repositoryService.getActiveLayerFromSelectedTile().getBrush();
+		Rectangle selection = service.getSelection();
 		int tileWidth = conf.tileWidth;
+		int loops = tile.isMulticolorEnabled() ? 2 : 1;
+		flip(action, content, tileWidth, selection, loops);
+		if (brush != null && brush.length > 0) {
+			flip(action, brush, tileWidth, selection, loops);
+		}
+
+	}
+
+	private void flip(int action, int[] data, int tileWidth, Rectangle selection, int loops) {
 		if (action == HORIZONTAL) {
-			for (int y = r.y; y < r.y + r.height; y++) {
-				for (int x = r.x, c = 0; x < r.x + r.width / 2; x++, c++) {
-					int a = content[x + (y * tileWidth)];
-					int b = content[r.x + r.width - 1 - c + (y * tileWidth)];
-					content[x + (y * tileWidth)] = b;
-					content[r.x + r.width - 1 - c + (y * tileWidth)] = a;
+			for (int y = selection.y; y < selection.y + selection.height; y++) {
+				for (int x = selection.x, c = 0; x < selection.x + selection.width / 2; x++, c++) {
+					int a = data[x + (y * tileWidth)];
+					int b = data[selection.x + selection.width - 1 - c + (y * tileWidth)];
+					data[x + (y * tileWidth)] = b;
+					data[selection.x + selection.width - 1 - c + (y * tileWidth)] = a;
 				}
 			}
 		} else if (action == VERTICAL) {
-			for (int y = r.y, c = 0; y < r.y + r.height / 2; y++, c++) {
-				for (int x = r.x; x < r.x + r.width; x++) {
-					int a = content[x + (y * tileWidth)];
-					int b = content[x + ((r.y + r.height - c - 1) * tileWidth)];
-					content[x + (y * tileWidth)] = b;
-					content[x + ((r.y + r.height - c - 1) * tileWidth)] = a;
+			for (int y = selection.y, c = 0; y < selection.y + selection.height / 2; y++, c++) {
+				for (int x = selection.x; x < selection.x + selection.width; x++) {
+					int a = data[x + (y * tileWidth)];
+					int b = data[x + ((selection.y + selection.height - c - 1) * tileWidth)];
+					data[x + (y * tileWidth)] = b;
+					data[x + ((selection.y + selection.height - c - 1) * tileWidth)] = a;
 				}
 			}
 		}
 	}
-
 }
--- de.drazil.nerdsuite/src/de/drazil/nerdsuite/imaging/service/RotationService.java
+++ de.drazil.nerdsuite/src/de/drazil/nerdsuite/imaging/service/RotationService.java
@@ -26,36 +26,47 @@ public class RotationService extends AbstractImagingService {
 
 	@Override
 	public void each(int action, int tileIndex, Tile tile, TileRepositoryService repositoryService, TileAction tileAction) {
-		Rectangle r = service.getSelection();
+		Rectangle selection = service.getSelection();
 		int[] content = repositoryService.getActiveLayerFromSelectedTile().getContent();
-		int[] contentSelection = new int[r.width * r.height];
-		int[] targetContentSelection = new int[r.width * r.height];
+		int[] brush = repositoryService.getActiveLayerFromSelectedTile().getBrush();
+		int[] sourceContentSelection = new int[selection.width * selection.height];
+		int[] sourceBrushSelection = new int[selection.width * selection.height];
+		int[] targetContentSelection = new int[selection.width * selection.height];
+		int[] targetBrushSelection = new int[selection.width * selection.height];
 		int tileWidth = conf.getTileWidth();
+		int loops = tile.isMulticolorEnabled() ? 2 : 1;
+		rotate(action, content, sourceContentSelection, targetContentSelection, tileWidth, selection, loops);
+		if (brush != null && brush.length > 0) {
+			rotate(action, brush, sourceBrushSelection, targetBrushSelection, tileWidth, selection, loops);
+		}
 
-		for (int x = r.x, cx = 0; x < r.x + r.width; x++, cx++) {
-			for (int y = r.y, cy = 0; y < r.y + r.height; y++, cy++) {
-				contentSelection[cx + cy * r.width] = content[x + y * tileWidth];
+	}
+
+	private void rotate(int action, int[] data, int[] source, int[] target, int tileWidth, Rectangle selection, int loops) {
+		for (int x = selection.x, cx = 0; x < selection.x + selection.width; x++, cx++) {
+			for (int y = selection.y, cy = 0; y < selection.y + selection.height; y++, cy++) {
+				source[cx + cy * selection.width] = data[x + y * tileWidth];
 			}
 		}
 
-		for (int y = 0; y < r.height; y++) {
-			for (int x = 0; x < r.width; x++) {
-				int b = contentSelection[x + (y * r.width)];
+		for (int y = 0; y < selection.height; y++) {
+			for (int x = 0; x < selection.width; x++) {
+				int b = source[x + (y * selection.width)];
 				int o = 0;
 				if (action == CCW) {
-					o = (targetContentSelection.length) - (r.width) - (r.width * x) + y;
+					o = (target.length) - (selection.width) - (selection.width * x) + y;
 				} else if (action == CW) {
-					o = (r.width) - y - 1 + (x * r.width);
+					o = (selection.width) - y - 1 + (x * selection.width);
 				}
-				if (o >= 0 && o < (targetContentSelection.length)) {
-					targetContentSelection[o] = b;
+				if (o >= 0 && o < (target.length)) {
+					target[o] = b;
 				}
 			}
 		}
 
-		for (int x = r.x, cx = 0; x < r.x + r.width; x++, cx++) {
-			for (int y = r.y, cy = 0; y < r.y + r.height; y++, cy++) {
-				content[x + y * tileWidth] = targetContentSelection[cx + cy * r.width];
+		for (int x = selection.x, cx = 0; x < selection.x + selection.width; x++, cx++) {
+			for (int y = selection.y, cy = 0; y < selection.y + selection.height; y++, cy++) {
+				data[x + y * tileWidth] = target[cx + cy * selection.width];
 			}
 		}
 	}
